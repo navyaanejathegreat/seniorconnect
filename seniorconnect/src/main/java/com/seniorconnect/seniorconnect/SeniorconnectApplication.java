8e58f7762b4747c81eae2390e35ed5a9ab2b52a6
navyaanejathegreat <navyaaneja25@gmail.com>
2026-04-12 19:26:46 +0530

    commit

--- seniorconnect/src/main/java/com/seniorconnect/seniorconnect/SeniorconnectApplication.java
+++ seniorconnect/src/main/java/com/seniorconnect/seniorconnect/SeniorconnectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SeniorconnectApplication {
+public class 	SeniorconnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeniorconnectApplication.class, args);
